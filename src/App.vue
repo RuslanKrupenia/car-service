354321dcc9ec4077c85d0d225c22ee0e66277510
RuslanKrupenia <110058918+RuslanKrupenia@users.noreply.github.com>
2023-07-19 17:06:33 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="app">
+  <Header />
+  <About/>
+  <Profit/>
+  <Services/>
+  <ReviewSection/>
+  <Works/>
+  <FeedbackForm/>
+  <!-- <MapComponent/> -->
+  
+</div>
+</template>
+
+<script>
+import Header from './components/Header';
+import About from './components/About';
+import Profit from './components/Profit';
+import Services from './components/Services'
+import ReviewSection from './components/ReviewSection.vue'
+import Works from './components/Works.vue'
+import FeedbackForm from './components/FeedbackForm.vue'
+// import MapComponent from './components/MapComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,About,Profit,Services,Works,FeedbackForm,ReviewSection,
+    // MapComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
